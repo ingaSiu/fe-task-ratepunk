ca80fedefc05d73b62ce4210fed575324a2df614
ingaSiu <ingasiud@gmail.com>
2023-06-14 21:04:10 +0300

    structure added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import MainContent from '../components/MainContent/MainContent';
+import Nav from '../components/Nav/Nav';
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <MainContent />
+    </div>
+  );
+};
+
+export default Home;
