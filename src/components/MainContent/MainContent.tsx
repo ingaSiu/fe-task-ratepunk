dab7221a78f46d3b35719ce8a80c91c6869d6734
ingaSiu <ingasiud@gmail.com>
2023-06-15 21:32:19 +0300

    components added

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -3,7 +3,16 @@ import StepInfo from './StepInfo/StepInfo';
 import styles from './MainContent.module.scss';
 
 const MainContent = () => {
-  return <div className={styles.container}>MainContent</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.left_container}>
+        <Form />
+      </div>
+      <div className={styles.right_container}>
+        <StepInfo />
+      </div>
+    </div>
+  );
 };
 
 export default MainContent;
