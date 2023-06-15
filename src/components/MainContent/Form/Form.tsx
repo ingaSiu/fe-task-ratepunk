93d6ac6ba9a231ca604a16f00753ab04fd7b4216
ingaSiu <ingasiud@gmail.com>
2023-06-15 21:31:53 +0300

    form content added

--- src/components/MainContent/Form/Form.tsx
+++ src/components/MainContent/Form/Form.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import Button from '../../Button/Button';
+import styles from './Form.module.scss';
+import { useState } from 'react';
 
 const Form = () => {
-  return <div>Form</div>;
+  const [email, setEmail] = useState('');
+  const [referral, setReferral] = useState('');
+
+  const handleSubmit = () => {
+    setReferral('https://ratepunk.com/referral');
+  };
+
+  const handleCopy = (e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(referral);
+  };
+  return (
+    <div className={styles.form_wrapper}>
+      <div className={styles.title}>REFER FRIENDS AND GET REWARDS</div>
+      <div className={styles.information}>
+        Refer your friends to us and earn hotel booking vouchers. We'll give you 1 coin for each friend that installs
+        our extension. Minimum cash-out at 20 coins.
+      </div>
+      <form>
+        <div>Error msg</div>
+
+        <input
+          type="text"
+          value={email}
+          placeholder="Enter your email address"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button />
+      </form>
+
+      <form>
+        <div>Success message</div>
+        <div className={styles.input_with_btn}>
+          <input type="text" value={referral} onChange={(e) => setReferral(e.target.value)} />
+          <button onClick={handleCopy}>Copy</button>
+        </div>
+      </form>
+
+      <p className={styles.message}>Limits on max rewards apply.</p>
+    </div>
+  );
 };
 
 export default Form;
