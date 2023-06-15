14a170b005ccc7295d135f49d6255e4094bdaaf4
ingaSiu <ingasiud@gmail.com>
2023-06-15 21:30:52 +0300

    components added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
+import Footer from '../components/Footer/Footer';
 import MainContent from '../components/MainContent/MainContent';
 import Nav from '../components/Nav/Nav';
+import StoreContent from '../components/StoreContent/StoreContent';
 
 const Home = () => {
   return (
     <div>
       <Nav />
       <MainContent />
+      <StoreContent />
+      <Footer />
     </div>
   );
 };
