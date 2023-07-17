236f8664ca33e70a3de2b38973dac25404f1e84d
ingaSiu <ingasiud@gmail.com>
2023-07-17 09:10:58 +0300

    reusable step card component

---
+++ src/components/MainContent/StepInfo/StepCard.tsx
@@ -0,0 +1,28 @@
+import styles from './StepInfo.module.scss';
+
+type StepProps = {
+  img: string;
+  altImg: string;
+  count: string;
+  title: string;
+  info: string;
+};
+
+const StepCard = ({ img, altImg, count, title, info }: StepProps) => {
+  return (
+    <>
+      <div className={styles.step}>
+        <div className={styles.img_wrapper}>
+          <img src={img} alt={altImg} />
+        </div>
+        <div className={styles.info_wrapper}>
+          <div className={styles.count}>{count}</div>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.info}>{info}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StepCard;
