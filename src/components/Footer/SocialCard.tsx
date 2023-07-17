68fc4f8175a7c2fce43f9482f1bcab069d2c1446
ingaSiu <ingasiud@gmail.com>
2023-07-17 09:10:26 +0300

    reusable social card component

---
+++ src/components/Footer/SocialCard.tsx
@@ -0,0 +1,18 @@
+import styles from './Footer.module.scss';
+
+type SocialProps = {
+  src: string;
+  alt: string;
+};
+
+const SocialCard = ({ src, alt }: SocialProps) => {
+  return (
+    <div className={styles.img_wrapper}>
+      <a href="#">
+        <img src={src} alt={alt} />
+      </a>
+    </div>
+  );
+};
+
+export default SocialCard;
