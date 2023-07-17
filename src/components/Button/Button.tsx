609ecdf30d82d0eb97879417a50306eb95dead33
ingaSiu <ingasiud@gmail.com>
2023-07-17 07:53:43 +0300

    del unnecessary prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,11 @@
 import styles from './Button.module.scss';
 
-// TODO
-// padaryti vietoj name text arba tiesiog name istrinti ir ideti teksta kaip childen, nes ten
-// ne pats pavadinimas o labiau tiesiog tekstas ant btn
 type ButtonProps = {
-  name: string;
   children?: React.ReactNode;
 };
 
-const Button = ({ name, children }: ButtonProps) => {
-  return (
-    <button className={styles.btn}>
-      {name}
-      {children}
-    </button>
-  );
+const Button = ({ children }: ButtonProps) => {
+  return <button className={styles.btn}>{children}</button>;
 };
 
 export default Button;
