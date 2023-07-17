8771258748f93b392d3d8ea1a7395c0e985a4c73
ingaSiu <ingasiud@gmail.com>
2023-07-17 09:10:03 +0300

    change to reusable component

--- src/components/StoreContent/StoreContent.tsx
+++ src/components/StoreContent/StoreContent.tsx
@@ -1,34 +1,22 @@
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillStar } from 'react-icons/ai';
+import ContentCard from './ContentCard';
 import styles from './StoreContent.module.scss';
-
-// padaryti atskira componenta ir per props paduoti tuos vardus
 
 const StoreContent = () => {
   return (
     <div className={styles.wrapper}>
-      <a
-        href="https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk"
-        target="_blank"
-      >
-        <div className={styles.store_container}>
-          <img src="chrome.svg" alt="Chrome icon" />
-
-          <div>
-            <p className={styles.text}>available in the</p>
-            <p className={styles.store_name}>chrome web store</p>
-          </div>
-        </div>
-      </a>
-      <a href="https://apps.apple.com/app/ratepunk/id1607823726" target="_blank">
-        <div className={styles.store_container}>
-          <img src="apple.svg" alt="Apple icon" />
-
-          <div>
-            <p className={styles.text}>available in the</p>
-            <p className={styles.store_name}>apple app store</p>
-          </div>
-        </div>
-      </a>
+      <ContentCard
+        link="https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk"
+        name="chrome.svg"
+        text="available in the"
+        storeName="chrome web store"
+      />
+      <ContentCard
+        link="https://apps.apple.com/app/ratepunk/id1607823726"
+        name="apple.svg"
+        text="available in the"
+        storeName="apple app store "
+      />
       <div className={styles.reviews_container}>
         <div className={styles.icon_wrapper}>
           <AiFillStar />
