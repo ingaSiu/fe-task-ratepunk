ebed87f74760bee9241ad85d0364fb93f30a7471
ingaSiu <ingasiud@gmail.com>
2023-07-26 11:02:42 +0300

    mobile menu added to nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,20 +1,21 @@
+import MobileMenu from './MobileMenu/MobileMenu';
 import styles from './Nav.module.scss';
 
-const Nav = () => {
-  return (
-    <div className={styles.nav}>
-      <div className={styles.left_container}>
-        <img src="logo.svg" alt="Ratepunk logo" />
-      </div>
-      <div className={styles.right_container}>
-        <div className={styles.links_wrapper}>
-          <a href="#">Chrome Extension</a>
-          <a href="#">Price Comparision</a>
-          <a href="#">Blog</a>
-        </div>
+const Nav = () => (
+  <div className={styles.nav}>
+    <div className={styles.leftContainer}>
+      <img src="logo.svg" alt="Ratepunk logo" />
+    </div>
+    <div className={styles.rightContainer}>
+      <div className={styles.linksWrapper}>
+        <a href="#">Chrome Extension</a>
+        <a href="#">Price Comparision</a>
+        <a href="#">Blog</a>
       </div>
     </div>
-  );
-};
+
+    <MobileMenu />
+  </div>
+);
 
 export default Nav;
