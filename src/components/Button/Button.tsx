c5df7915c722e8b6155e2642cb1edbd9946c7c19
ingaSiu <ingasiud@gmail.com>
2023-08-01 17:07:52 +0300

    added on

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,13 @@ import styles from './Button.module.scss';
 
 type ButtonProps = {
   children?: React.ReactNode;
+  onClick?: (event: React.SyntheticEvent) => void;
 };
 
-const Button = ({ children }: ButtonProps) => (
-   <button className={styles.btn}>{children}</button>
+const Button = ({ children, onClick }: ButtonProps) => (
+  <button className={styles.btn} onClick={onClick}>
+    {children}
+  </button>
 );
 
 export default Button;
