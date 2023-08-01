cadf889cdcb149148076b5062c94da69d95f33f7
ingaSiu <ingasiud@gmail.com>
2023-08-01 17:08:32 +0300

    referral form with functionality and style

---
+++ src/components/MainContent/LeftSection/Form/referral/Referral.tsx
@@ -0,0 +1,29 @@
+import { SyntheticEvent, useRef } from 'react';
+
+import Button from '../../../../Button/Button';
+import styles from './Referral.module.scss';
+
+const Referral: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const value = 'https://ratepunk.com/referral';
+
+  const handleCopy = (e: SyntheticEvent) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(value);
+  };
+  return (
+    <>
+      <div className={styles.inputWithBtn}>
+        <input type="text" ref={inputRef} value={value} readOnly />
+        <button onClick={handleCopy}>Copy</button>
+      </div>
+
+      <div className={styles.mobileBtn}>
+        <Button onClick={handleCopy}>Copy URL</Button>
+      </div>
+    </>
+  );
+};
+
+export default Referral;
