9e998c769760b34e430e0a99bb281dbda3ed48b6
ingaSiu <ingasiud@gmail.com>
2023-08-19 16:49:37 +0300

    add prop to btn

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,6 +3,7 @@ import styles from './Button.module.scss';
 type ButtonProps = {
   children?: React.ReactNode;
   onClick?: (event: React.SyntheticEvent) => void;
+  disabled: boolean;
 };
 
 const Button = ({ children, onClick }: ButtonProps) => (
