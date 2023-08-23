21fcc9159617667e75cb51c82406fd41284b4f24
ingaSiu <ingasiud@gmail.com>
2023-08-23 19:38:57 +0300

    small fix to type

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,11 @@ import styles from './Button.module.scss';
 type ButtonProps = {
   children?: React.ReactNode;
   onClick?: (event: React.SyntheticEvent) => void;
-  disabled: boolean;
+  disabled?: boolean;
 };
 
 const Button = ({ children, onClick }: ButtonProps) => (
-  <button className={styles.btn} onClick={onClick}>
+  <button className={styles.btn} onClick={onClick} disabled={false}>
     {children}
   </button>
 );
